91ea09b75cceb91a6edfdbb6a279eae11bf2100c
dmartynov <denis.martynov.93@mail.ru>
2019-06-15 21:32:54 +0700

    upd server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', (req, res) => {
 	res.sendFile('index.html', { root : __dirname});
