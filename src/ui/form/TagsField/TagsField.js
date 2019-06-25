aba62be62ce8a1548e136860e2ce76c105379402
Vladimir Kozhin <hello@kozhindev.com>
2019-06-25 11:13:11 +0700

    upd TagField

--- src/ui/form/TagsField/TagsField.js
+++ src/ui/form/TagsField/TagsField.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _uniq from 'lodash-es/uniq';
-import _includes from 'lodash-es/includes';
-import { InputField } from 'yii-steroids/ui/form';
+import {InputField} from 'yii-steroids/ui/form';
 import fieldHoc from 'yii-steroids/ui/form/fieldHoc';
 import dataProviderHoc from 'yii-steroids/ui/form/dataProviderHoc';
 import enhanceWithClickOutside from 'react-click-outside';
 
-import { html } from 'components';
+import {html} from 'components';
 
 const bem = html.bem('TagsField');
 import './TagsField.scss';
+import _remove from 'lodash/remove';
 
-export default
-@fieldHoc({
+export default @fieldHoc({
     componentId: 'form.TagsField',
 })
 @dataProviderHoc()
@@ -74,20 +73,12 @@ class TagsField extends React.PureComponent {
         }
     }
 
-    componentDidMount() {
-        window.addEventListener('keydown', this._onKeyDown);
-    }
-
     componentWillReceiveProps(nextProps) {
         // Add by dataProviderHoc on enter click
         if (this.props.input.value !== nextProps.input.value && this.props.isOpened) {
-            this.setState({ value: '' });
+            this.setState({value: ''});
             this.props.onClose();
         }
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this._onKeyDown);
     }
 
     getValues() {
@@ -106,32 +97,10 @@ class TagsField extends React.PureComponent {
                             input={{
                                 value: this.state.value,
                                 onChange: this._onChange,
-                                onFocus: this._onFocus,
+                                onKeyDown: this._onKeyDown
                             }}
                         />
                     </div>
-                    {this.props.isOpened && (
-                        <div className={bem.element('drop-down')}>
-                            <div className={bem.element('drop-down-list')}>
-                                {this.props.items
-                                    .filter(item => !_includes(this.getValues(), item.id))
-                                    .map(item => (
-                                        <div
-                                            key={item.id}
-                                            className={bem.element('drop-down-item', {
-                                                hover: this.props.hoveredItem && this.props.hoveredItem.id === item.id,
-                                                select: !!this.props.selectedItems.find(selectedItem => selectedItem.id === item.id),
-                                            })}
-                                            onClick={() => this._onItemClick(item)}
-                                            onMouseOver={() => this.props.onItemMouseOver(item)}
-                                        >
-                                            <span>{item.label}</span>
-                                            <span>{item.total}</span>
-                                        </div>
-                                    ))}
-                            </div>
-                        </div>
-                    )}
                 </div>
                 {this.getValues().length > 0 && (
                     <div className={bem.element('skills')}>
@@ -148,7 +117,7 @@ class TagsField extends React.PureComponent {
                                             bem.element('skill-remove'),
                                             'Icon Icon__close-small'
                                         )}
-                                        onClick={() => this._onRemove(value)}
+                                        onClick={e => this._onRemove(e, value)}
                                     />
                                 </li>
                             ))}
@@ -161,25 +130,14 @@ class TagsField extends React.PureComponent {
 
     _onAdd(value) {
         if (value) {
-            this.setState({ value: '' });
+            this.setState({value: ''});
             this.props.input.onChange(_uniq(this.getValues().concat(value)));
         }
     }
 
-    _onFocus() {
-        if (this.state.value && !this.props.isOpened) {
-            this.props.onOpen();
-            this.props.onSearch(this.state.value);
-        }
-    }
-
-    _onRemove(value) {
-        const skills = this.getValues();
-        const index = skills.indexOf(value);
-        if (index !== -1) {
-            skills.splice(index, 1);
-            this.props.input.onChange(skills);
-        }
+    _onRemove(e, value) {
+        e.preventDefault();
+        this.props.input.onChange(_remove(this.getValues(), tag => tag !== value));
     }
 
     _onChange(value) {
@@ -189,12 +147,7 @@ class TagsField extends React.PureComponent {
             }
             this.props.onSearch(value);
         }
-        this.setState({ value });
-    }
-
-    _onItemClick(item) {
-        this._onAdd(item.id);
-        this.props.onClose();
+        this.setState({value});
     }
 
     _onKeyDown(e) {
