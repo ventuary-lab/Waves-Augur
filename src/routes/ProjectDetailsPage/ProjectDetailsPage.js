5432bd22373c8c627fdd1128a234ebb2581ed0d3
looktoroot <>
2019-07-01 20:09:10 +0700

    style fixes

--- src/routes/ProjectDetailsPage/ProjectDetailsPage.js
+++ src/routes/ProjectDetailsPage/ProjectDetailsPage.js
@@ -29,101 +29,115 @@ export default class ProjectDetailsPage extends React.PureComponent {
                 <div className={bem.element('form-block')}>
                     <DonateForm/>
                 </div>
-                <div className={bem.element('leader-block')}>
-                    <div className={bem.element('title')}>
-                        {__('Leader')}
-                    </div>
+                <div className={bem.element('inner')}>
+                    <div className={bem.element('leader-block')}>
+                        <div className={bem.element('title')}>
+                            {__('Leader')}
+                        </div>
 
-                    {/*<UserCard
+                        {/*<UserCard
                         item={this.props.project.author}
                     />*/}
-                    <UserCard
-                        item={{
-                            profile: {
-                                name: 'Aleksei Pupyshev',
-                                title: 'Founder & CEO @Ventuary',
-                                tags: ['Consulting', 'RND', 'Analytics'],
-                                avatar: '',
-                                country: 'Russia',
-                            }
-                        }}
-                    />
-                </div>
-                <div className={bem.element('content')}>
-                    <div className={'row'}>
-                        <div className={'col col_tablet-count-8'}>
-                            <div className={bem.element('title')}>
-                                {__('Expenses')}
+                        <UserCard
+                            item={{
+                                profile: {
+                                    name: 'Aleksei Pupyshev',
+                                    title: 'Founder & CEO @Ventuary',
+                                    tags: ['Consulting', 'RND', 'Analytics'],
+                                    avatar: '',
+                                    country: 'Russia',
+                                }
+                            }}
+                        />
+                    </div>
+                    <div className={bem.element('content')}>
+                        <div className={'row'}>
+                            <div className={'col col_desk-reverse col_desk-count-5'}>
+                                <div className={bem.element('links')}>
+                                    <div className={bem.element('social-links')}>
+                                        <SocialLinks
+                                            urls={{
+                                                url_twitter: '1',
+                                                url_facebook: '1',
+                                                url_linkedin: '1',
+                                                url_instagram: '1',
+                                                url_telegram: '1',
+                                                url_website: '1',
+                                            }}
+                                        />
+                                    </div>
+                                    <a
+                                        className={bem.element('share-link')}
+                                        href={'javascript:void(0)'}
+                                    >
+                                        {__('Share Project')}
+                                    </a>
+                                </div>
                             </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
-                            </p>
-                            <div className={bem.element('title')}>
-                                {__('Problem')}
+                            <div className={'col col_desk-count-7'}>
+                                <div className={bem.element('info')}>
+                                    <div className={bem.element('title')}>
+                                        {__('Expenses')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
+                                    </p>
+                                    <div className={bem.element('title')}>
+                                        {__('Problem')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner. ')}
+                                    </p>
+                                    <div className={bem.element('title')}>
+                                        {__('MVP')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
+                                    </p>
+                                    <div className={bem.element('title')}>
+                                        {__('X-Factor')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
+                                    </p>
+                                    <div className={bem.element('title')}>
+                                        {__('Large Scale Adoption')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
+                                    </p>
+                                    <div className={bem.element('title')}>
+                                        {__('Impact On User')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
+                                    </p>
+                                    <div className={bem.element('title')}>
+                                        {__('Impact On User Context')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
+                                    </p>
+                                    <div className={bem.element('title')}>
+                                        {__('Code Validation')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
+                                    </p>
+                                    <div className={bem.element('title')}>
+                                        {__('Impact On User Context')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
+                                    </p>
+                                    <div className={bem.element('title')}>
+                                        {__('Legal Arrangements')}
+                                    </div>
+                                    <p className={bem.element('description')}>
+                                        {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
+                                    </p>
+                                </div>
                             </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner. ')}
-                            </p>
-                            <div className={bem.element('title')}>
-                                {__('MVP')}
-                            </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
-                            </p>
-                            <div className={bem.element('title')}>
-                                {__('X-Factor')}
-                            </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
-                            </p>
-                            <div className={bem.element('title')}>
-                                {__('Large Scale Adoption')}
-                            </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
-                            </p>
-                            <div className={bem.element('title')}>
-                                {__('Impact On User')}
-                            </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
-                            </p>
-                            <div className={bem.element('title')}>
-                                {__('Impact On User Context')}
-                            </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
-                            </p>
-                            <div className={bem.element('title')}>
-                                {__('Code Validation')}
-                            </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
-                            </p>
-                            <div className={bem.element('title')}>
-                                {__('Impact On User Context')}
-                            </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
-                            </p>
-                            <div className={bem.element('title')}>
-                                {__('Legal Arrangements')}
-                            </div>
-                            <p className={bem.element('description')}>
-                                {__('Confidence and reliability The decentralized system guarantees the payment to the carrier regardless of IMMLA&rsquo;s financial condition, the same way it guarantees the delivery or insurance payment to the cargo owner.')}
-                            </p>
-                        </div>
-                        <div className={'col col_tablet-count-4'}>
-                            <SocialLinks
-                                urls={{
-                                    url_twitter: '1',
-                                    url_facebook: '1',
-                                    url_linkedin: '1',
-                                    url_instagram: '1',
-                                    url_telegram: '1',
-                                    url_website: '1',
-                                }}
-                            />
                         </div>
                     </div>
                 </div>
