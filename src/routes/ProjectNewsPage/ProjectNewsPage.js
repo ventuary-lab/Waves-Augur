815360e19598518c80d28d59d2f306bde0771a47
Vladimir Kozhin <hello@kozhindev.com>
2019-07-04 18:31:27 +0700

    fix news page

--- src/routes/ProjectNewsPage/ProjectNewsPage.js
+++ src/routes/ProjectNewsPage/ProjectNewsPage.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import _get from 'lodash/get';
 
-import {dal, html, resource} from 'components';
+import {html, resource} from 'components';
 
 import ProjectSchema from 'types/ProjectSchema';
 import './ProjectNewsPage.scss';
 
 const bem = html.bem('ProjectNewsPage');
 
-@dal.hoc(
-    props => dal.getProject(_get(props, 'match.params.uid'))
-        .then(project => ({project}))
-)
 export default class ProjectNewsPage extends React.PureComponent {
 
     static propTypes = {
@@ -20,15 +14,12 @@ export default class ProjectNewsPage extends React.PureComponent {
     };
 
     componentDidMount() {
-
-        // const twitterName = this.props.project.socials.url_twitter.match(/\/[A-Za-z0-9]+$/)[0].substring(1);
-
+        const twitterName = this.props.project.socials.url_twitter.match(/\/[A-Za-z0-9]+$/)[0].substring(1);
         resource.loadTwitterWidget().then(twttr => {
             twttr.widgets.createTimeline(
                 {
                     sourceType: 'profile',
-                    screenName: 'MarsCuriosity',
-                    // screenName: twitterName,
+                    screenName: twitterName,
                 },
                 this.refs.twitter
             );
@@ -36,7 +27,6 @@ export default class ProjectNewsPage extends React.PureComponent {
     }
 
     render() {
-
         return (
             <div className={bem.block()}>
                 <div className={bem.element('twitter')}>
@@ -46,8 +36,7 @@ export default class ProjectNewsPage extends React.PureComponent {
                         data-tweet-limit={7}
                         data-chrome='nofooter'
                         data-link-color='#0368ff'
-                        href='https://twitter.com/MarsCuriosity'
-                        // href={this.props.project.socials.url_twitter}
+                        href={this.props.project.socials.url_twitter}
                     >
                         Tweets by @MarsCuriosity
                     </a>
