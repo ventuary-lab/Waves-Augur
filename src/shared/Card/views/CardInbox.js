cb1af6f5deef9d5d2731ce4cb4ebf90b073b3eec
dmartynov <denis.martynov.93@mail.ru>
2019-07-08 15:05:18 +0700

    add link to project in ProjectInboxCard

--- src/shared/Card/views/CardInbox.js
+++ src/shared/Card/views/CardInbox.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import InboxTypeEnum from 'enums/InboxTypeEnum';
+import Link from 'yii-steroids/ui/nav/Link';
 
 import {html} from 'components';
 
 import './CardInbox.scss';
+import {ROUTE_PROJECT_DETAILS} from 'routes';
 
 const bem = html.bem('CardInbox');
 
 export default class CardInbox extends React.PureComponent {
 
     static propTypes = {
-        type: PropTypes.oneOf(InboxTypeEnum.getKeys())
+        type: PropTypes.oneOf(InboxTypeEnum.getKeys()),
+        uid: PropTypes.string,
+        address: PropTypes.string,
+        isProject: PropTypes.bool,
     };
 
     render() {
@@ -19,9 +24,21 @@ export default class CardInbox extends React.PureComponent {
             <div className={bem.block()}>
 
                 <div className={bem.element('info')}>
-                    <div className={bem.element('icon')}>
+                    <Link
+                        className={bem.element('icon')}
+                        toRoute={
+                            this.props.isProject
+                                ? ROUTE_PROJECT_DETAILS
+                                : null
+                        }
+                        toRouteParams={{
+                            uid: this.props.isProject && this.props.uid,
+                            address: !this.props.isProject && this.props.address
+                        }}
+                        noStyles
+                    >
                         <span className={`Icon ${InboxTypeEnum.getCssClass(this.props.type)}`}/>
-                    </div>
+                    </Link>
                     <div className={bem.element('title', {
                         gray: this.props.type === InboxTypeEnum.PROPOSAL_REJECTED || this.props.type === InboxTypeEnum.YOUR_PROPOSAL_REJECTED,
                     })}>
