60fb1325cc153ba3d85ef3c211775a0a2ec185b5
looktoroot <>
2019-07-09 16:42:39 +0700

    fix twitter name

--- src/routes/ProjectNewsPage/ProjectNewsPage.js
+++ src/routes/ProjectNewsPage/ProjectNewsPage.js
@@ -14,12 +14,11 @@ export default class ProjectNewsPage extends React.PureComponent {
     };
 
     componentDidMount() {
-        const twitterName = this.props.project.socials.url_twitter.match(/\/[A-Za-z0-9]+$/)[0].substring(1);
         resource.loadTwitterWidget().then(twttr => {
             twttr.widgets.createTimeline(
                 {
                     sourceType: 'profile',
-                    screenName: twitterName,
+                    screenName: this.getTwitterName(),
                 },
                 this.refs.twitter
             );
@@ -38,10 +37,14 @@ export default class ProjectNewsPage extends React.PureComponent {
                         data-link-color='#0368ff'
                         href={this.props.project.socials.url_twitter}
                     >
-                        Tweets by @MarsCuriosity
+                        {`Tweets by @${this.getTwitterName()}`}
                     </a>
                 </div>
             </div>
         );
+    }
+
+    getTwitterName() {
+        return this.props.project.socials.url_twitter.match(/\/[A-Za-z0-9]+$/)[0].substring(1);
     }
 }
