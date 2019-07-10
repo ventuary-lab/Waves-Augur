1c2bfbe79e267df705ddeebb610bdd0eee415b4c
dmartynov <denis.martynov.93@mail.ru>
2019-07-10 11:58:19 +0700

    add HeaderMobileMenu

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -5,6 +5,7 @@ import {getNavUrl} from 'yii-steroids/reducers/navigation';
 import Link from 'yii-steroids/ui/nav/Link';
 import Form from 'yii-steroids/ui/form/Form';
 import InputField from 'yii-steroids/ui/form/InputField';
+import enhanceWithClickOutside from 'react-click-outside';
 
 import {html} from 'components';
 import InputSearchView from 'shared/Header/views/InputSearchView';
@@ -12,6 +13,7 @@ import {ROUTE_ROOT} from 'routes';
 import HeaderNav from './views/HeaderNav';
 import HeaderProfile from './views/HeaderProfile';
 import HeaderHamburger from './views/HeaderHamburger';
+import HeaderMobileMenu from './views/HeaderMobileMenu';
 import logoSvg from 'static/icons/logo.svg';
 
 import './Header.scss';
@@ -23,18 +25,32 @@ const bem = html.bem('Header');
         indexPageUrl: getNavUrl(state, ROUTE_ROOT),
     })
 )
+@enhanceWithClickOutside
 export default class Header extends React.PureComponent {
 
     static propTypes = {
         indexPageUrl: PropTypes.string,
     };
 
+    constructor() {
+        super(...arguments);
+
+        this.state = {
+            isMenuOpen: false,
+        };
+
+        this.closeMenu = this.closeMenu.bind(this);
+    }
+
     render() {
         return (
             <header className={bem.block()}>
                 <div className={bem.element('inner')}>
                     <div className={bem.element('hamburger')}>
-                        <HeaderHamburger/>
+                        <HeaderHamburger
+                            onClick={() => this.setState({isMenuOpen: !this.state.isMenuOpen})}
+                            isActive={this.state.isMenuOpen}
+                        />
                     </div>
                     <Link
                         className={bem.element('logo')}
@@ -64,8 +80,20 @@ export default class Header extends React.PureComponent {
                         <HeaderProfile/>
                     </div>
                 </div>
+                <div className={bem.element('menu', {active: this.state.isMenuOpen})}>
+                    <HeaderMobileMenu
+                        onClose={this.closeMenu}
+                    />
+                </div>
             </header>
         );
     }
 
+    handleClickOutside() {
+        this.closeMenu();
+    }
+
+    closeMenu() {
+        this.setState({isMenuOpen: false});
+    }
 }
---
+++ src/shared/Header/views/HeaderMobileMenu.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import Link from 'yii-steroids/ui/nav/Link';
+import {getNavItems} from 'yii-steroids/reducers/navigation';
+import InputField from 'yii-steroids/ui/form/InputField';
+import Form from 'yii-steroids/ui/form/Form';
+
+import {html} from 'components';
+import InputFieldHamburgerSearchView from './InputFieldHamburgerSearchView';
+import NavItemSchema from 'types/NavItemSchema';
+import {ROUTE_ROOT} from 'routes';
+
+import './HeaderMobileMenu.scss';
+
+const bem = html.bem('HeaderMobileMenu');
+
+@connect(
+    state => ({
+        navItems: getNavItems(state, ROUTE_ROOT),
+    })
+)
+export default class HeaderMobileMenu extends React.PureComponent {
+
+    static propTypes = {
+        navItems: PropTypes.arrayOf(NavItemSchema),
+        onClose: PropTypes.func,
+    };
+
+    render() {
+        return (
+            <div className={bem.block()}>
+                <ul className={bem.element('list')}>
+                    {this.props.navItems.filter(item => item.isNavVisible !== false).map(navItem => (
+                        <li
+                            key={navItem.id}
+                            className={bem.element('item')}
+                        >
+                            <Link
+                                className={bem.element('link', {
+                                    'active': navItem.isActive,
+                                })}
+                                to={navItem.url}
+                                label={navItem.label}
+                                onClick={this.props.onClose}
+                                noStyles
+                            />
+                        </li>
+                    ))}
+                </ul>
+                <div className={bem.element('form')}>
+                    <Form formId='HeaderMenuSearch'>
+                        <InputField
+                            attribute='search'
+                            placeholder={__('Search')}
+                            view={InputFieldHamburgerSearchView}
+                        />
+                    </Form>
+                </div>
+            </div>
+        );
+    }
+}
