e654befe96b7e930b0b14a34d68db11cc0b79bea
Shamil Khalilov <shamfama@gmail.com>
2019-07-27 14:47:43 +0300

    Fix route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+
 app.use(function(req, res, next) {
     if (req.secure){
         return next();
@@ -11,10 +11,13 @@ app.use(function(req, res, next) {
     res.redirect("https://" + req.headers.host + req.url);
 });
 
+app.use(express.static(__dirname + '/dist'));
+
 app.get('/*', (req, res) => {
     res.sendFile('index.html', { root : __dirname + '/dist'});
 });
 let port = process.env.PORT || 5000;
+
 app.listen(port, () => {
     console.log(__dirname);
     console.log("Listening Port " + port);
