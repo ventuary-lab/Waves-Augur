c786058b73b0a597661b25a6370e4da5d2059b3f
Shamil Khalilov <shamfama@gmail.com>
2019-08-15 00:56:00 +0300

    Fix

--- webpack.js
+++ webpack.js
@@ -37,12 +37,11 @@ require('yii-steroids/webpack')
             },
             plugins: [
                 new webpack.DefinePlugin({
-                    APP_DAPP_NETWORK: process.env.APP_DAPP_NETWORK,
-                    DAPP: process.env.DAPP,
-                    NODE_ENV: process.env.NODE_ENV,
-                    NODE_URL: process.env.NODE_URL,
-                    ORACLE: process.env.ORACLE,
-                    ORACLE_SEED: process.env.ORACLE_SEED,
+                    'process.env.APP_DAPP_NETWORK': JSON.stringify(process.env.APP_DAPP_NETWORK),
+                    'process.env.DAPP': JSON.stringify(process.env.DAPP),
+                    'process.env.NODE_URL': JSON.stringify(process.env.NODE_URL),
+                    'process.env.ORACLE': JSON.stringify(process.env.ORACLE),
+                    'process.env.ORACLE_SEED': JSON.stringify(process.env.ORACLE_SEED),
                     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
                 })
             ]
