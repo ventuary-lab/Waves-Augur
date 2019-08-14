0138aae76cffe1708621215f587733fbb7f74206
Shamil Khalilov <shamfama@gmail.com>
2019-08-15 01:07:22 +0300

    Fix

--- webpack.js
+++ webpack.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
 
+console.log('process.env.DAPP', process.env.DAPP);
+
 require('yii-steroids/webpack')
     .config({
         port: 8081,
@@ -42,7 +44,8 @@ require('yii-steroids/webpack')
                     'process.env.NODE_URL': JSON.stringify(process.env.NODE_URL),
                     'process.env.ORACLE': JSON.stringify(process.env.ORACLE),
                     'process.env.ORACLE_SEED': JSON.stringify(process.env.ORACLE_SEED),
-                    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+                    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+                    NODE_ENV: JSON.stringify(process.env.NODE_ENV),
                 })
             ]
         },
