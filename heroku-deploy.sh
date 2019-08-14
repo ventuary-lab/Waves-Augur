22fba9813a000ce16b4d2c9c9315ae4fdb8a1940
Shamil Khalilov <shamfama@gmail.com>
2019-08-14 23:17:03 +0300

    Upd .env

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,4 +1,7 @@
 # !/env/bash
 
+cat .env
+ls -la
+
 docker build -t ventuary .
-docker run -itd --name ventuary --restart always -v ~/dev/ventuary:/data -p 5000:5000 ventuary
+docker run --env-file=.env -itd --name ventuary --restart always -v ~/dev/ventuary:/data -p 5000:5000 ventuary
