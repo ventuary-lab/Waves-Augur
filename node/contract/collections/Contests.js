200fc46af6b879ecd440167c3edf6871e8acd5a0
Shamil Khalilov <shamfama@gmail.com>
2019-08-26 19:53:52 +0300

    Rework getContests filter

--- node/contract/collections/Contests.js
+++ node/contract/collections/Contests.js
@@ -33,7 +33,10 @@ module.exports = class Contests extends BaseCollection {
         switch (filterName) {
             case ContestFilter.FEATURED:
                 //TODO
-                // contests = contests.filter(item => item.status === ProjectStatusEnum.CROWDFUND);
+                contests = contests.filter(item => (
+                    moment(item.expireEntries)
+                        .isAfter(moment.utc())
+                ));
                 // contests = _orderBy(contests, 'positiveBalance', 'desc');
                 break;
 
