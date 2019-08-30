58d5d5485d1246db849500173ecebd52c6c078db
Shamil Khalilov <shamfama@gmail.com>
2019-08-30 14:33:28 +0300

    Fix props

--- src/modals/ProjectWizardModal/views/ProjectWizard.js
+++ src/modals/ProjectWizardModal/views/ProjectWizard.js
@@ -45,6 +45,8 @@ export default class ProjectWizard extends React.PureComponent {
     }
 
     render() {
+        const { project } = this.props;
+
         return (
             <div className={bem.block()}>
                 <FormWizard
