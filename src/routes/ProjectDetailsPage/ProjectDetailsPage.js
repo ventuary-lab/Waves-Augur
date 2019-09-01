323f2f6f103e237ca0a094c5076707da2a4b9b00
Shamil Khalilov <shamfama@gmail.com>
2019-09-01 18:56:39 +0300

    Fix preview count

--- src/routes/ProjectDetailsPage/ProjectDetailsPage.js
+++ src/routes/ProjectDetailsPage/ProjectDetailsPage.js
@@ -23,7 +23,7 @@ export default class ProjectDetailsPage extends React.PureComponent {
         return (
             <div className={bem.block()}>
                 {/* <ProjectPreviewDetails previews={this.props.project.previews || []}/> */}
-                <ProjectPreviewDetails previews={[...this.props.project.previews, ...this.props.project.previews, ...this.props.project.previews] || []}/>
+                <ProjectPreviewDetails previews={this.props.project.previews || []}/>
                 <div className={bem.element('leader-block')}>
                     <div className={bem.element('title')}>
                         {__('Leader')}
