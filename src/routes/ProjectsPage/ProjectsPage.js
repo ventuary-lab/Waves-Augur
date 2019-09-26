2108066ccdade38ebd46bd1d5e8d083b9da64b6f
Shamil Khalilov <shamfama@gmail.com>
2019-09-26 23:02:03 +0300

    Rework lazy load

--- src/routes/ProjectsPage/ProjectsPage.js
+++ src/routes/ProjectsPage/ProjectsPage.js
@@ -30,28 +30,13 @@ export default class ProjectsPage extends React.PureComponent{
         this._getLayout = this._getLayout.bind(this);
         this._loadMore = this._loadMore.bind(this);
         this._loadData = this._loadData.bind(this);
+        this._getInitialState = this._getInitialState.bind(this);
         this._handleDataToStateConcat = this._handleDataToStateConcat.bind(this);
-
-        this.isFeed = this.props.match.params.state === ProjectStateEnum.FEED;
-
-        this.requestConfig = (
-            this.isFeed ? {
-                url: '/api/v1/reviews/donations',
-                key: 'donations',
-                collection: 'reviewDonations',
-            } : {
-                url: `/api/v1/projects/${props.match.params.state}`,
-                key: 'projects',
-                collection: 'projects',
-            }
-        );
+        this.cancelToken = axios.CancelToken;
+        this.tokenSource = this.cancelToken.source();
 
         this.state = {
-            currentPage: 0,
-            isLoading: false,
-            hasMore: false,
-            donations: [],
-            projects: []
+            ...this._getInitialState()
         };
     }
 
@@ -67,9 +52,38 @@ export default class ProjectsPage extends React.PureComponent{
         };
     }
 
+    _getRequestConfig (props = this.props) {
+        return (
+            this._checkIsFeed() ? {
+                url: '/api/v1/reviews/donations',
+                key: 'donations',
+                collection: 'reviewDonations',
+            } : {
+                url: `/api/v1/projects/${props.match.params.state}`,
+                key: 'projects',
+                collection: 'projects',
+            }
+        );
+    }
+
+    _checkIsFeed (props = this.props) {
+        return props.match.params.state === ProjectStateEnum.FEED;
+    }
+
+    _getInitialState (props = this.props) {
+        return {
+            currentPage: 0,
+            isLoading: false,
+            hasMore: false,
+            donations: [],
+            projects: [],
+            isFeed: this._checkIsFeed(props),
+            requestConfig:  { ...this._getRequestConfig(props) }
+        };
+    }
+
     async _loadData () {
-        const { isFeed } = this;
-        const { isLoading } = this.state;
+        const { isLoading, requestConfig, isFeed, currentPage } = this.state;
 
         if (isLoading) {
             return;
@@ -77,11 +91,27 @@ export default class ProjectsPage extends React.PureComponent{
 
         this.setState({ isLoading: true });
 
-        const response = await axios.get(this.requestConfig.url, {
-            params: {
-                page: this.state.currentPage
-            }
-        });
+        let response;
+
+        try {
+            response = await axios.get(requestConfig.url, {
+                cancelToken: this.tokenSource.token,
+                params: {
+                    page: currentPage
+                }
+            });
+        } catch (err) {
+            if (axios.isCancel(err)) {
+                console.log('Request canceled', err.message);
+            };
+
+            this.tokenSource = this.cancelToken.source();
+
+            this.setState({ isLoading: false });
+            return;
+        }
+
+        this.setState({ isLoading: false });
 
 
         this.setState(prevState => ({ ...prevState, isLoading: false, currentPage: prevState.currentPage + 1 }));
@@ -94,6 +124,20 @@ export default class ProjectsPage extends React.PureComponent{
         }
 
         this.setState(prevState => this._handleDataToStateConcat(prevState, data, isFeed ? 'donations' : 'projects'));
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (this.props.match.params.state !== newProps.match.params.state) {
+            if (this.state.isLoading) {
+                this.tokenSource.cancel('Operation cancelled!');
+            }
+            console.log({ newProps });
+
+            this.setState({ ...this._getInitialState(newProps) });
+
+            console.log({ st: this.state });
+            this._loadData();
+        }
     }
 
     async componentDidMount () {
@@ -115,7 +159,7 @@ export default class ProjectsPage extends React.PureComponent{
                     </div>
                 </div>
             </section>
-        )
+        );
 
         if (!donations && !projects) {
             return loader;
@@ -126,6 +170,7 @@ export default class ProjectsPage extends React.PureComponent{
                 pageStart={currentPage}
                 loadMore={this._loadMore}
                 hasMore={hasMore}
+                initialLoad={false}
                 loader={loader}>
                 {this._getLayout()}
             </InfiniteScroll>
@@ -133,8 +178,7 @@ export default class ProjectsPage extends React.PureComponent{
     }
 
     _getLayout () {
-        const { isFeed } = this;
-        const { projects, donations } = this.state;
+        const { projects, donations, isFeed } = this.state;
 
         return (
             <section className={bem.block()}>
