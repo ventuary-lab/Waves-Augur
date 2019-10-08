0224934ea07103d88328e0d4e9322be87f10ce40
Shamil Khalilov <shamfama@gmail.com>
2019-10-08 22:21:27 +0300

    Add confirm dialog

--- src/shared/TransactionApproveModal/index.js
+++ src/shared/TransactionApproveModal/index.js
@@ -19,6 +19,10 @@ import './index.scss';
 
 const bem = html.bem('TransactionApproveModal');
 
+const INITIAL_VIEW = 'initialView';
+const REQUEST_SUCCESS_VIEW = 'requestSuccessView';
+const REQUEST_FAIL_VIEW = 'requestFailView';
+
 class TransactionApproveModal extends React.Component {
     constructor (props) {
         super(props);
@@ -27,7 +31,8 @@ class TransactionApproveModal extends React.Component {
         this._getView = this._getView.bind(this);
 
         this.state = {
-            isVisible: true
+            isVisible: true,
+            currentView: INITIAL_VIEW
         };
     }
 
@@ -36,6 +41,7 @@ class TransactionApproveModal extends React.Component {
     }
 
     _getView () {
+        const { currentView } = this.state;
         const onPasswordChange = () => {};
         const {
             methodName,
@@ -47,31 +53,48 @@ class TransactionApproveModal extends React.Component {
             fee: '0.009 WAVES'
         };
 
+        const badge = (
+            currentView === REQUEST_SUCCESS_VIEW && (
+                <AlertBadge text='The transaction was successfully confirmed'/>
+            ) || 
+            currentView === REQUEST_FAIL_VIEW && (
+                <AlertBadge text='The transaction was denied' type='fail'/>
+            )
+        );
+        const inputs = currentView === INITIAL_VIEW && (
+            <>
+                <BaseInput
+                    label='Enter your password'
+                    icon={eyeIcon}
+                    type='password'
+                    onChange={onPasswordChange}
+                />
+                <div className={bem.element('action-btn-cont')}>
+                    <Button
+                        type='submit'
+                        color='primary'
+                        label='Reject'
+                        onClick={() => this.setState({ currentView: REQUEST_FAIL_VIEW })}
+                        outline
+                    />
+                    <Button
+                        type='submit'
+                        color='primary'
+                        label='Approve'
+                        onClick={() => this.setState({ currentView: REQUEST_SUCCESS_VIEW })}
+                    />
+                </div>
+            </>
+        );
+
         return (
             <div className={bem.element('base-view')}>
                 <div className={bem.element('top-heading')}>
                     <h4>Confirmation request</h4>
                 </div>
                 <div className={bem.element('card-body')}>
-                    <BaseInput
-                        label='Enter your password'
-                        icon={eyeIcon}
-                        type='password'
-                        onChange={onPasswordChange}
-                    />
-                    <div className={bem.element('action-btn-cont')}>
-                        <Button
-                            type='submit'
-                            color='primary'
-                            label='Reject'
-                            outline
-                        />
-                        <Button
-                            type='submit'
-                            color='primary'
-                            label='Approve'
-                        />
-                    </div>
+                    {badge}
+                    {inputs}
                     <div className={bem.element('additional-info')}>
                         <span className='title'>Function:</span>
                         <span>{methodName}</span>
@@ -80,6 +103,14 @@ class TransactionApproveModal extends React.Component {
                         <span className='title'>Fee</span>
                         <span>{fee}</span>
                     </div>
+                    {currentView !== INITIAL_VIEW && (
+                        <Button
+                            type='submit'
+                            color='primary'
+                            label='Ok'
+                            onClick={() => this.setState({ currentView: INITIAL_VIEW })}
+                        />
+                    )}
                 </div>
             </div>
         )
